df93e039c53a24251ba4014da4e2a9a6eebd08da
suprojegrubu <suprojegrubu@gmail.com>
2025-10-24 09:47:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        return render_template('index.html')
     return render_template('index.html')
 
 @app.route('/moreabout', methods=['POST'])
 def moreabout():
     return render_template('page2.html')
 
-
-@app.route('/', methods=['POST'])
-def gotoindex():
-    return render_template('index.html')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
