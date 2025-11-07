9e833ad28032c4783fd6fa662a640079dd235498
suprojegrubu <suprojegrubu@gmail.com>
2025-11-07 15:17:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-class Applicant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    message = db.Column(db.Text, nullable=True)
-
-@app.route('/apply', methods=['POST'])
-def apply():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form.get('message', '')
-
-    new_applicant = Applicant(name=name, email=email, message=message)
-    db.session.add(new_applicant)
-    db.session.commit()
-
-    return redirect('/') 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -41,10 +18,6 @@ def hikaye():
 @app.route('/kaynakca', methods=['POST'])
 def kaynakca():
     return render_template('page4.html')
-
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
